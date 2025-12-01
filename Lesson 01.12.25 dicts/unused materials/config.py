f4082a62290a4ab6dff2be67dfa4349c5fd428ba
silksherstkaw <silksherstkaw@yandex.ru>
2025-12-01 21:03:41 +0300

    added materials from 1 dec 2025 lecture

---
+++ Lesson 01.12.25 dicts/unused materials/config.py
@@ -0,0 +1,8 @@
+config = {
+    "model_name": "bert-base-uncased",
+    "batch_size": 32,
+    "max_length": 128,
+    "learning_rate": 2e-5,
+    "epochs": 3,
+    "labels": ["positive", "negative", "neutral"]
+}
