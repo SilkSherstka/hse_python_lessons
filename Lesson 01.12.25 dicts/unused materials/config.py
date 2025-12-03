276a25f716be236ed32f97f64e8bc672216e6437
SilkSherstka <silksherstkaw@yandex.ru>
2025-12-04 01:48:08 +0300

    Delete Lesson 01.12.25 dicts directory
    deleted wrong folder

--- Lesson 01.12.25 dicts/unused materials/config.py
+++
@@ -1,8 +0,0 @@
-config = {
-    "model_name": "bert-base-uncased",
-    "batch_size": 32,
-    "max_length": 128,
-    "learning_rate": 2e-5,
-    "epochs": 3,
-    "labels": ["positive", "negative", "neutral"]
-}
